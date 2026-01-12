4470ba688fbf155ea92fbef691422e236eefddfe
Hidaya Mohamed <hidayamohamed002@gmail.com>
2026-01-13 02:28:56 +0300

    Complete lab

--- server/app.py
+++ server/app.py
@@ -10,8 +10,7 @@ class ClearSession(Resource):
 
     def delete(self):
     
-        session['page_views'] = None
-        session['user_id'] = None
+        session.clear()
 
         return {}, 204
 
@@ -25,19 +24,43 @@ class Signup(Resource):
         user.password_hash = json['password']
         db.session.add(user)
         db.session.commit()
+
+        session['user_id'] = user.id
         return user.to_dict(), 201
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        user_id = session.get('user_id')
+
+        if user_id:
+            user = User.query.get(user_id)
+            return user.to_dict(), 200
+
+        return {}, 204
 
 class Login(Resource):
-    pass
+    def post(self):
+        data = request.get_json()
+        user = User.query.filter_by(username=data['username']).first()
+
+        if user and user.authenticate(data['password']):
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+
+        return {}, 401
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        session.pop('user_id', None)
+        return {}, 204
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
 
+
+api.add_resource(CheckSession, '/check_session')
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(port=5000, debug=True)
